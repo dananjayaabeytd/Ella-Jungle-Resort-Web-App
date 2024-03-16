ca86059013e37845ab1553d83bd600f3f3e6ef00
Tharindu Dushan <tdmahindarathne@gmail.com>
2024-03-16 17:00:03 +0530

    Update Residence-homepage.jsx

--- frontend/src/pages/Residence-homepage/Residence-homepage.jsx
+++ frontend/src/pages/Residence-homepage/Residence-homepage.jsx
@@ -3,7 +3,6 @@ import BackgroundText from "./residence-home-component/BackgroundText";
 function ResidenceHome() {
     return (
       <div>
-        <h1 class="text-4xl text-red-600 font-bold">This is Home Page</h1>
         <BackgroundText/>
       </div>
     );
