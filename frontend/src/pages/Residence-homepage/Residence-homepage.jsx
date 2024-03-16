13a92da9b6edbb0c3ebab05281b6ea6500bc3990
Tharindu Dushan <tdmahindarathne@gmail.com>
2024-03-16 22:16:07 +0530

    Room card added

--- frontend/src/pages/Residence-homepage/Residence-homepage.jsx
+++ frontend/src/pages/Residence-homepage/Residence-homepage.jsx
@@ -1,9 +1,27 @@
 import BackgroundText from "./residence-home-component/BackgroundText";
+import RoomCard from "./residence-home-component/RoomCard";
 
 function ResidenceHome() {
     return (
       <div>
         <BackgroundText/>
+        <div class="my-4 flex justify-between mx-40">
+        <RoomCard
+          imageUrl="https://wallup.net/wp-content/uploads/2016/05/24/306644-nature.jpg"
+          Title="Room 1"
+          Description="This is a room"
+        />
+        <RoomCard
+          imageUrl="https://wallup.net/wp-content/uploads/2016/05/24/306644-nature.jpg"
+          Title="Room 1"
+          Description="This is a room"
+        />
+        <RoomCard
+          imageUrl="https://wallup.net/wp-content/uploads/2016/05/24/306644-nature.jpg"
+          Title="Room 1"
+          Description="This is a room"
+        />
+        </div>
       </div>
     );
   }
