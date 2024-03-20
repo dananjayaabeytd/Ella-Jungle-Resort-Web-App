4cad7332b739d9a0d148037a1ad0dab63466d8b5
SayuniJayanetti <sayunijayanetti5@gmail.com>
2024-03-20 08:50:29 +0530

    Backend for the first crud
    Created the backend for the crud related to the admin, where admin add, view, update or delete special activities.

---
+++ BACKEND/routes/SpecialActivity.js
@@ -0,0 +1,99 @@
+//import router functions in express package
+const router = require("express").Router();
+let SpecialActivity = require("../models/SpecialActivity");
+
+
+router.route("/add").post((req,res)=>{
+    const name=req.body.name;
+    const image=req.body.image;
+    const description = req.body.description;
+    const price =Number(req.body.price);
+
+    const newSpecialActivity = new SpecialActivity({
+        name,
+        image,
+        description,
+        price
+    })
+
+
+     //send the values of the above properties to the db
+     newSpecialActivity.save().then(()=>{
+        res.json("Special Activity added")
+    }).catch((err)=>{
+        console.log(err);
+    })
+
+})
+
+//to view all the values added
+router.route("/").get((req,res)=>{
+    SpecialActivity.find().then((SpecialActivity)=>{
+        res.json(pecialActivity)
+    }).catch((err)=>{
+        console.log(err)
+    })
+})
+
+
+//to update values
+router.route("/update/:id").put(async(req,res)=>{
+
+    //fetch the id comes as a parameter in the request
+    let userId = req.params.id;
+    
+    //fetching the newly updated data in destructive format
+    const{name,image,description,price}=req.body;
+
+    //creating an object to update the data
+    const updateSpecialActivity={
+        name,
+        image,
+        description,
+        price
+    }
+    
+
+    //to check whether an activity exists related to a specific id
+    const update = await SpecialActivity.findByIdAndUpdate(userId,updateSpecialActivity)
+    .then(()=>{
+        res.status(200).send({status:"Special Activity updated"})
+    }).catch((err)=>{
+        console.log(err);
+        res.status(500).send({status:"Error with updating data",error:err.message});
+    })
+
+})
+
+
+
+//to delete a user
+router.route("/delete/:id").delete(async(req,res)=>{
+    let userId = req.params.id;
+
+    await SpecialActivity.findByIdAndDelete(userId).then(()=>{
+        res.status(200).send({status:"Special Activity deleted"});
+    }).catch((err)=>{
+        console.log(err.message);
+        res.status(500).send({status:"Error with deleting special actvities", error:err.message});
+    })
+})
+
+
+
+
+//to retreive data of a one activity
+router.route("/get/:id").get(async(req,res)=>{
+    let userId = req.params.id;
+
+    const user = await SpecialActivity.findById(userId)
+    .then((SpecialActivity)=>{
+        res.status(200).send({status:"Special Activity fetched",SpecialActivity})
+    }).catch(()=>{
+        console.log(err.message);
+        res.status(500).send({status:"Error with get activity", error: err.message});
+    })
+})
+
+
+module.exports = router;
---
+++ BACKEND/server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+
+
+//define the port
+const PORT = process.env.PORT || 8080;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+//get the db link
+const URL = process.env.MONGODB_URL;
+
+
+//mongodb configuration
+//connect to our db using the given url and the options provided
+mongoose.connect(URL,{
+    
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+   
+}
+);
+
+//open the db connection we created
+const connection = mongoose.connection;
+connection.once("open",()=>{
+    console.log("Mongodb Connection Success!");
+});
+
+
+//import students.js file to access students.js router file
+const specialActivityRouter = require("./routes/SpecialActivity.js");
+
+app.use("/SpecialActivity",specialActivityRouter);
+
+
+//load our app in the port
+app.listen(PORT,()=>{
+    console.log(`Server is up & running on port $(PORT)`);
+})
