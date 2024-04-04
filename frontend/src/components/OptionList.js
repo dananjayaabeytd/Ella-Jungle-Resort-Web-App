cc0282881db5a02d01cf6b05002dbfcb12906cbe
VilanSiriwardana <vilansiriwardana@gmail.com>
2024-04-04 15:37:39 +0530

    Display Options with Images

--- frontend/src/components/OptionList.js
+++ frontend/src/components/OptionList.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
-import bggreen from '../assets/bggreen.jpg'; // Import the image
-import { Link } from "react-router-dom"; // Import Link from react-router-dom
 import axios from "axios";
+import { Link } from "react-router-dom";
+import bggreen from '../assets/bggreen.jpg'; // Import the image
 
 export default function OptionList() {
+    
   const [allOptions, setOption] = useState([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedOptionId, setSelectedOptionId] = useState(null);
@@ -39,167 +40,92 @@ export default function OptionList() {
     }
   };
 
-  return (
-    <div className="relative min-h-screen">
-      {/* Background Image */}
-      <div
-        className="absolute inset-0 z-0 bg-fixed"
-        style={{
-          backgroundImage: `url(${bggreen})`,
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-        }}
-      ></div>
-  
-      {/* Content Wrapper */}
-      <div className="relative z-10 flex flex-col items-center justify-center min-h-screen">
-        <Link to="/addOption" className="ml-5 mb-6 text-white text-xl font-mclaren px-4 py-1 bg-blue-500 hover:bg-blue-800 rounded-3xl items-center">
+    // Get unique categories
+    const categories = [...new Set(allOptions.map((option) => option.optionCategory))];
+
+    return (
+        <div className="relative min-h-screen">
+            {/* Background Image */}
+            <div
+                className="absolute inset-0 z-0 bg-fixed"
+                style={{
+                    backgroundImage: `url(${bggreen})`,
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                }}
+            ></div>
+
+            {/* Content Wrapper */}
+            <div className="relative z-10 flex flex-col justify-items-stretch mr-20 ml-20 min-h-screen p-2 items-center justify-between">
+
+            <Link to="/addOption" className="ml-5 mb-6 text-white text-xl font-mclaren px-4 py-1 bg-blue-500 hover:bg-blue-800 rounded-3xl items-center">
           Add Option
         </Link>
-  
-        {/* Your scrolling content */}
-        <div className="container my-5 max-w-5xl mx-auto p-5 bg-white bg-opacity-50 shadow-2xl rounded-3xl overflow-auto border-2 border-green-700">
-        <div className="grid grid-cols-2 gap-16 sm:grid-cols-2 lg:grid-cols-1 xl:grid-cols-1">
-        
-      
-        </div>
-            <div className="px-2">
-                <table className="table-auto w-full">
-                    
-                    <tr>
-                        <th className="px-4 py-2 text-left">Option</th>
-                        <th className="px-4 py-2 text-left">Description</th>
-                        <th className="px-4 py-2 text-left" >Price</th>
-                        <th className="m-4 py-2" colSpan="2">Actions</th>
-                    </tr>
-                    
-                    <tbody>
-                    <tr >
-                        <th className="text-xl font-bold text-green-800 font-inika pt-4" colSpan="5">Catering Options</th>
-                    </tr>
-                        {allOptions && allOptions.filter((option) => option.optionCategory === "Catering").map((option) => (
-          
-                        <tr key={option._id}>
-                            <td className="text-md font-bold text-green-800 font-inika">{option.optionName}</td>
-                            <td className="text-sm font-mclaren">{option.optionDescription} </td>
-                            <td className="text-md font-bold text-blue-600">{option.optionPrice} LKR</td> 
-                            <td>
-                              <Link to={`/updateOption/${option._id}`} 
-                              className=" text-black text-xs font-mclaren px-3 py-0   bg-theme-green hover:bg-green-800 rounded-3xl">Update</Link>
-                            </td>
-                                
-                                <td><button className="ml-5 text-black text-xs font-mclaren px-3 py-0  bg-red-500 hover:bg-red-800 rounded-3xl" onClick={() => handleDeleteClick(option._id)}>
-                                  Remove
-                                </button></td>   
-                        </tr>  
-                    ))}
-                    <tr >
-                        <th className="text-xl font-bold text-green-800 font-inika pt-4" colSpan="5">Entertainment Options</th>
-                    </tr>
-                        {allOptions && allOptions.filter((option) => option.optionCategory === "Entertainment").map((option) => (
-          
-                        <tr key={option._id}>
-                            <td className="text-md font-bold text-green-800 font-inika">{option.optionName}</td>
-                            <td className="text-sm font-mclaren">{option.optionDescription}</td>
-                            <td className="text-md font-bold text-blue-600">{option.optionPrice} LKR</td> 
-                            <td>
-                              <Link to={`/updateOption/${option._id}`} 
-                              className=" text-black text-xs font-mclaren px-3 py-0   bg-theme-green hover:bg-green-800 rounded-3xl">Update</Link>
-                            </td>
-                                <td><button className="ml-5 text-black text-xs font-mclaren px-3 py-0   bg-red-500 hover:bg-red-800 rounded-3xl" onClick={() => handleDeleteClick(option._id)}>
-                                  Remove
-                                </button></td>   
-                        </tr>  
-                    ))}
-    
-                    <tr >
-                        <th className="text-xl font-bold text-green-800 font-inika pt-4" colSpan="5">Parking Options</th>
-                    </tr>
-                        {allOptions && allOptions.filter((option) => option.optionCategory === "Parking").map((option) => (
-          
-                        <tr key={option._id}>
-                            <td className="text-sm font-bold text-green-800 font-inika">{option.optionName}</td>
-                            <td className="text-sm font-mclaren">{option.optionDescription}</td>
-                            <td className="text-md font-bold text-blue-600">{option.optionPrice} LKR</td>
-                            <td>
-                              <Link to={`/updateOption/${option._id}`} 
-                              className=" text-black text-xs font-mclaren px-3 py-0   bg-theme-green hover:bg-green-800 rounded-3xl">Update</Link>
-                            </td>
-                                <td><button className="ml-5 text-black text-xs font-mclaren px-3 py-0   bg-red-500 hover:bg-red-800 rounded-3xl" onClick={() => handleDeleteClick(option._id)}>
-                                  Remove
-                                </button></td>   
-                        </tr>  
-                    ))}
-    
-                    
-    <tr >
-                        <th className="text-xl font-bold text-green-800 font-inika pt-4" colSpan="5">Photography Options</th>
-                    </tr>
-                        {allOptions && allOptions.filter((option) => option.optionCategory === "Photography").map((option) => (
-          
-                        <tr key={option._id}>
-                            <td className="text-md font-bold text-green-800 font-inika">{option.optionName}</td>
-                            <td className="text-sm font-mclaren">{option.optionDescription}</td>
-                            <td className="text-md font-bold text-blue-600">{option.optionPrice} LKR</td>
-                            <td>
-                              <Link to={`/updateOption/${option._id}`} 
-                              className=" text-black text-xs font-mclaren px-3 py-0   bg-theme-green hover:bg-green-800 rounded-3xl">Update</Link>
-                            </td>
-                                <td><button className="ml-5 text-black text-xs font-mclaren px-3 py-0   bg-red-500 hover:bg-red-800 rounded-3xl" onClick={() => handleDeleteClick(option._id)}>
-                                  Remove
-                                </button></td>   
-                        </tr>  
-                    ))}
-    
-    
-                    
-    <tr >
-                        <th className="text-xl font-bold text-green-800 font-inika pt-4" colSpan="5">Other Options</th>
-                    </tr>
-                        {allOptions && allOptions.filter((option) => option.optionCategory === "Other").map((option) => (
-          
-                        <tr key={option._id}>
-                            <td className="text-md font-bold text-green-800 font-inika">{option.optionName}</td>
-                            <td className="text-sm font-mclaren">{option.optionDescription}</td>
-                            <td className="text-md font-bold text-blue-600">{option.optionPrice} LKR</td>
-                            <td>
-                              <Link to={`/updateOption/${option._id}`} 
-                              className=" text-black text-xs font-mclaren px-3 py-0  bg-theme-green hover:bg-green-800 rounded-3xl">Update</Link>
-                            </td>
-                                <td><button className="ml-5 text-black text-xs font-mclaren px-3 py-0   bg-red-500 hover:bg-red-800 rounded-3xl" onClick={() => handleDeleteClick(option._id)}>
-                                  Remove
-                                </button></td>   
-                        </tr>  
-                    ))}
-                    </tbody>
-                </table>
-    
+
+                {/* Loop through categories */}
+                {categories.map((category, index) => (
+                    <div key={index}>
+                        {/* Category Title */}
+                        <div className="container mt-16 max-w-5xl mx-auto">
+                            <p className="bg-secondary-green rounded-3xl p-1 text-2xl font-bold text-green-800 font-inika text-center">
+                                {category} Options
+                            </p>
+                        </div>
+                        {/* Cards for each category */}
+                        <div className="items-center justify-between grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4  w-full p-2">
+                            {/* Cards for this category */}
+                            {allOptions
+                                .filter((option) => option.optionCategory === category)
+                                .map((option) => (
+                                    <div key={option._id} className="bg-white max-w-md max-h-ful px-4 m-4 rounded-t-md rounded-b-3xl border-secondary-green border-double border-8 overflow-hidden shadow-2xl hover:scale-up-110">
+                                        <div className="container shadow-md rounded-3xl overflow-hidden w-full max-h-80">
+                                            <img className="w-full h-full object-fill mt-3" src={`http://localhost:8070/Images/${option.optionImage}`} alt="Event" />
+                                        </div>
+                                        <div className="px-0 mb-3 mt-2 rounded-xl">
+                                            <div className="pb-1">
+                                                <p className="text-lg font-bold text-green-800 font-inika text-center">{option.optionName}</p>
+                                            </div>
+                                            <p className="text-sm font-mclaren text-center">{option.optionDescription}</p>
+                                            <p className="text-base text-blue-700 font-bold font-mclaren text-center pt-2 pb-1">{option.optionPrice} LKR</p>
+                                            <div className="mb-4 mt-1 flex justify-center items-center">
+                                                {/* Using Link component for Update button */}
+                                                <Link
+                                                    to={`/updateOption/${option._id}`}
+                                                    className="text-white text-base font-mclaren px-4 py-0 bg-theme-green hover:bg-green-800 rounded-3xl"
+                                                >
+                                                    Update
+                                                </Link>
+                                                <button
+                                                    className="ml-5 text-white text-base font-mclaren px-4 py-0 bg-red-500 hover:bg-red-800 rounded-3xl"
+                                                    onClick={() => handleDeleteClick(option._id)}
+                                                >
+                                                    Remove
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                ))}
+                        </div>
+                    </div>
+                ))}
+
+
                 
-                <div className="price ">
-                </div>
-      
-                {/* Event Description with McLaren font */}
-                </div>
-      
-                <div className="flex justify-center items-center">
-                          
-                </div>
-  
-          {/* Pop-up Modal for deletion confirmation */}
-          {isModalOpen && (
+        {/* Pop-up Modal for deletion confirmation */}
+        {isModalOpen && (
             <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full" id="my-modal">
-              <div className="relative top-1/3  mx-auto p-5 border w-96 shadow-lg rounded-3xl bg-white roun">
+              <div className="relative top-1/3  mx-auto p-5 w-96 shadow-lg rounded-3xl bg-white border-secondary-green " >
                 <div className="mt-3 text-center">
                   <h3 className="leading-6 text-xl font-bold text-green-800 font-inika pt-4">Removal Confirmation</h3>
                   <div className="mt-2 px-7 py-3">
-                    <p className="text-sm font-mclaren text-gray-500">
+                    <p className="text-md font-mclaren text-gray-500">
                       Are you sure you want to remove this option? This action cannot be undone</p>
                   </div>
                   <div className="items-center px-4 py-3">
                     <button id="delete-btn" 
                     className="px-4 py-2 mx-2 font-mclaren bg-red-500 text-white text-base font-medium rounded-lg w-24 shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-opacity-50" 
                     onClick={confirmDelete}>
-                      Delete
+                      Remove
                     </button>
                     <button 
                     className="px-4 py-2 mx-2 font-mclaren bg-gray-500 text-white text-base font-medium rounded-lg w-24 shadow-sm hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-opacity-50" 
@@ -211,10 +137,9 @@ export default function OptionList() {
               </div>
             </div>
           )}
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 
