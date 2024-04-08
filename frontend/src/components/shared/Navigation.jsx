c4c578b5074adbf877554960c20838b5d3f93c07
Tharindu Dananjaya <dananjayaabeytd@gmail.com>
2024-04-08 09:41:08 +0530

    Minor changes in Header

--- frontend/src/components/shared/Navigation.jsx
+++ frontend/src/components/shared/Navigation.jsx
@@ -5,7 +5,6 @@ import {
   Typography,
   IconButton,
   Button,
-  Avatar, // Import Avatar component
 } from '@material-tailwind/react';
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 import logo from '../../assets/logo.png';
@@ -24,13 +23,11 @@ function NavList() {
         color='blue-gray'
         className='p-1 font-medium'
       >
-        <Link to='/'>
-          <a
-            href='#'
-            className='flex items-center font-bold transition-colors hover:text-green-500'
-          >
-            Home
-          </a>
+        <Link
+          to='/'
+          className='flex items-center font-bold transition-colors hover:text-green-500'
+        >
+          Home
         </Link>
       </Typography>
       <Typography
@@ -39,13 +36,11 @@ function NavList() {
         color='blue-gray'
         className='p-1 font-medium'
       >
-        <Link to='/agency'>
-          <a
-            href='#'
-            className='flex items-center font-bold transition-colors hover:text-green-500'
-          >
-            Agency
-          </a>
+        <Link
+          to='/agency'
+          className='flex items-center font-bold transition-colors hover:text-green-500'
+        >
+          Agency
         </Link>
       </Typography>
       <Typography
@@ -54,12 +49,12 @@ function NavList() {
         color='blue-gray'
         className='p-1 font-medium'
       >
-        <a
-          href='#'
+        <Link
+          to='#'
           className='flex items-center font-bold transition-colors hover:text-green-500'
         >
-          Blocks
-        </a>
+          Packages
+        </Link>
       </Typography>
       <Typography
         as='li'
@@ -67,12 +62,12 @@ function NavList() {
         color='blue-gray'
         className='p-1 font-medium'
       >
-        <a
-          href='#'
+        <Link
+          to='#'
           className='flex items-center font-bold transition-colors hover:text-green-500'
         >
-          Docs
-        </a>
+          About Us
+        </Link>
       </Typography>
     </ul>
   );
@@ -112,72 +107,8 @@ function MyNavbar() {
     <Navbar className='sticky top-0 z-50 px-6 py-3 mx-auto bg-white shadow-md my-7'>
       <div className='flex items-center justify-between text-blue-gray-900'>
         <img src={logo} alt='' className='h-10 w-25' />
-        <div className='hidden lg:flex lg:items-center lg:justify-center lg:block'>
-          <NavList />
-        </div>
-
+        
         <div className='flex items-center'>
-          {userInfo ? (
-            <>
-              <Typography
-                as='li'
-                variant='small'
-                color='blue-gray'
-                className='p-1 font-medium'
-              >
-                <Link
-                  to='/admindashboard'
-                  className='flex mr-5 font-bold transition-colors hover:text-green-500'
-                >
-                  {userInfo.isAdmin ? <h1>Admin</h1> : <h1></h1>}
-                </Link>
-              </Typography>
-              <Typography
-                as='li'
-                variant='small'
-                color='blue-gray'
-                className='p-1 font-medium'
-              >
-                <Link
-                  to='/profile'
-                  className='flex mr-5 font-bold transition-colors hover:text-green-500'
-                >
-                  {userInfo.name}
-                </Link>
-              </Typography>
-              <img
-                src={userInfo.img}
-                alt='avatar'
-                className='relative inline-block h-12 w-12 !rounded-full  object-cover object-center mr-5'
-              />
-              <Button color='green' size='regular' onClick={logoutHandler}>
-                Logout
-              </Button>
-            </>
-          ) : (
-            <>
-              <Link to='/sign-in'>
-                <Button
-                  color='green'
-                  size='regular'
-                  className='hidden mr-4 lg:block'
-                >
-                  Login
-                </Button>
-              </Link>
-
-              <Link to='/signup'>
-                <Button
-                  color='green'
-                  size='regular'
-                  className='hidden lg:block'
-                >
-                  Signup
-                </Button>
-              </Link>
-            </>
-          )}
-
           <IconButton
             variant='text'
             className='w-6 h-6 ml-4 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden'
@@ -191,6 +122,73 @@ function MyNavbar() {
             )}
           </IconButton>
         </div>
+        
+        <div className='hidden lg:flex lg:items-center lg:justify-center lg:block'>
+          <NavList />
+        </div>
+
+        {userInfo && (
+          <div className='flex items-center'>
+            <Typography
+              as='li'
+              variant='small'
+              color='blue-gray'
+              className='p-1 font-medium'
+            >
+              <Link
+                to='/admindashboard'
+                className='flex mr-5 font-bold transition-colors hover:text-green-500'
+              >
+                {userInfo.isAdmin && <h1>Admin</h1>}
+              </Link>
+            </Typography>
+            <Typography
+              as='li'
+              variant='small'
+              color='blue-gray'
+              className='p-1 font-medium'
+            >
+              <Link
+                to='/profile'
+                className='flex mr-5 font-bold transition-colors hover:text-green-500'
+              >
+                {userInfo.name}
+              </Link>
+            </Typography>
+            <img
+              src={userInfo.img}
+              alt='avatar'
+              className='relative inline-block h-12 w-12 !rounded-full  object-cover object-center mr-5'
+            />
+            <Button color='green' size='regular' onClick={logoutHandler}>
+              Logout
+            </Button>
+          </div>
+        )}
+
+        {!userInfo && (
+          <div className='hidden lg:flex lg:items-center'>
+            <Link to='/sign-in'>
+              <Button
+                color='green'
+                size='regular'
+                className='hidden mr-4 lg:block'
+              >
+                Login
+              </Button>
+            </Link>
+
+            <Link to='/signup'>
+              <Button
+                color='green'
+                size='regular'
+                className='hidden lg:block'
+              >
+                Signup
+              </Button>
+            </Link>
+          </div>
+        )}
       </div>
       <Collapse open={openNav}>
         <NavList />
